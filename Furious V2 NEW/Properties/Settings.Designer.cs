57300829ab1c7e4e4c74d307cd9e613d38653841
TrentonMetzler <treahaxor@gmail.com>
2024-01-26 23:37:14 -0600

    Working PS3 V2 release.

--- Furious V2 NEW/Properties/Settings.Designer.cs
+++ Furious V2 NEW/Properties/Settings.Designer.cs
@@ -8,22 +8,30 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Furious_V2_NEW.Properties
-{
-
-
+namespace Furious_V2_NEW.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("255, 10, 10")]
+        public string saveColor {
+            get {
+                return ((string)(this["saveColor"]));
+            }
+            set {
+                this["saveColor"] = value;
             }
         }
     }
---
+++ Furious V2 NEW/RoundLabel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+public class RoundLabel : Label
+{
+    protected override void OnPaint(System.Windows.Forms.PaintEventArgs e)
+    {
+        GraphicsPath grPath = new GraphicsPath();
+        grPath.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
+        this.Region = new System.Drawing.Region(grPath);
+        base.OnPaint(e);
+    }
+}
